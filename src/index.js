f6a159a4a45664f2d41cb225f1318b803ec3635a
Jamal <jshah@fluxworkstudios.com>
2020-11-10 22:34:18 -0500

    feat:  generate fake user

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import { name } from 'faker';
+import { name, random, internet, phone, lorem } from 'faker';
+import { getRandomInt } from './lib/utils';
 
-console.log(`Hello, ${name.firstName()}`);
+const user = {
+  id: random.uuid(),
+  firstName: name.firstName(),
+  lastName: name.lastName(),
+  email: internet.email(),
+  phoneNumber: phone.phoneNumber(),
+  welcomeTexts: [
+    ...Array(getRandomInt(2, 5))
+      .fill(null)
+      .map(() => lorem.sentence()),
+  ],
+};
+
+console.log(user);
