3ee087a8891cb22162689e74e9216c16beb62d9e
Jamal <jshah@fluxworkstudios.com>
2021-05-07 13:45:41 -0400

    fix:  use async in test script

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import { generateUser } from './lib/generators/user';
 import { recordMemoryUsage } from './lib/record-memory-usage';
 import { Timer } from './lib/timer';
 
-const users = generateUser(2e5);
+(async () => {
+  // generate 2,000,000 users
+  const users = generateUser(2e6);
 
-const timer = new Timer('user-generate');
-timer.start();
+  const timer = new Timer('user-generate');
+  timer.start();
 
-recordMemoryUsage('start');
+  recordMemoryUsage('start');
 
-const buffer = new BatchedBuffer(users, { bufferSize: 5000 });
-buffer.process((batch) => console.log(batch.length));
+  const buffer = new BatchedBuffer(users, { bufferSize: 2500 });
+  await buffer.process((batch) => {
+    recordMemoryUsage('processing');
+    console.log(`Created ${batch.length} users`);
+  });
 
-recordMemoryUsage('end');
+  recordMemoryUsage('end');
 
-timer.stop();
-console.log(timer.stringDuration);
+  timer.stop();
+  console.log(timer.stringDuration);
+})();
